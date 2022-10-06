089697b522f08e31ef6a9963b5516714951ca5c1
GabrielPw <gxaavier352@gmail.com>
2022-10-06 08:36:52 -0300

    apiEtecRpg

---
+++ AppRpgEtec/AppRpgEtec/Models/ClasseEnum.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppRpgEtec.Models
+{
+    internal class ClasseEnum
+    {
+    }
+}
---
+++ AppRpgEtec/AppRpgEtec/ViewModels/Personagens/ListagemPersonagemViewModel.cs
@@ -0,0 +1,24 @@
+﻿using AppRpgEtec.Models;
+using AppRpgEtec.Services.Personagens;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace AppRpgEtec.ViewModels.Personagens
+{
+    public class ListagemPersonagemViewModel : BaseViewModel
+    {
+
+        private PersonagemService pService;
+        public ObservableCollection<Personagem> Personagens { get; set; }
+
+        public ListagemPersonagemViewModel()
+        {
+            string token = "";
+            pService = new PersonagemService(token);
+
+            Personagens = new ObservableCollection<Personagem>();
+        }
+    }
+}
